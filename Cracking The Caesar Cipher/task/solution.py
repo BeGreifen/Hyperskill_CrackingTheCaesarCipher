99f10c1f53de094b215ddcc59e8f1b8f3b04ec72
gnubrew <b.greifen@gmail.com>
2023-07-29 09:29:37 +0200

    Initial commit

---
+++ Cracking The Caesar Cipher/task/solution.py
@@ -0,0 +1,10 @@
+import string
+
+# Write your code here
+
+def main():
+    raw_input = input()
+
+
+if __name__=="__main__":
+    main()
---
+++ Cracking The Caesar Cipher/task/tests.py
@@ -0,0 +1,3 @@
+from test.tests import DecipherTest
+
+if __name__ == '__main__':    DecipherTest().run_tests()
