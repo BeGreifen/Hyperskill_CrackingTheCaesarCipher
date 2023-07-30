e1ccf3316a85a1a0245f75e2b5dd3269729903c4
gnubrew <b.greifen@gmail.com>
2023-07-30 20:36:08 +0200

    o) level 3 + topics solutions

--- Cracking The Caesar Cipher/task/solution.py
+++ Cracking The Caesar Cipher/task/solution.py
@@ -25,27 +25,29 @@ def decorator(func):
 def output_result(result_list: list):
     result = "".join(result_list)
     print(result)
-    logging.info(result)
     return result
 
 
 # Example usage
 @decorator
 def string_list_to_numb(char_list: list) -> list:
-    logging.info(inspect.stack()[0][3])
-    logging.info(char_list)
     result = []
-    for char in char_list:
-        result.append(ord(char) - ord("a"))
+    if len(char_list) > 1:
+        for char in char_list:
+            result.append(ord(char) - ord("a"))
+    else:
+        result = ord(char_list[0]) - ord("a")
     return result
 
 
 @decorator
 def get_cesar(input_numb: list, offset=0) -> list:
     result_list = []
-    for numb in input_numb:
-        result_list.append((numb + offset) % 26)
-    logging.info(result_list)
+    if isinstance(input_numb, list):
+        for numb in input_numb:
+            result_list.append((numb + offset) % 26)
+    else:
+        result_list = (int(input_numb) + offset) % 26
     return result_list
 
 
@@ -53,17 +55,66 @@ def get_cesar(input_numb: list, offset=0) -> list:
 def numb_list_to_char_list(input_numb: list) -> list:
     result_list = []
     for numb in input_numb:
-        result_list.append(string.ascii_lowercase[int(numb)])
-    logging.info(result_list)
+        result_list.append(string.ascii_lowercase[numb])
     return result_list
 
 
-# Write your code here
+def create_dict(word: str) -> dict:
+    dict_find_shift = {}
+
+    for inti, _ in enumerate(string.ascii_lowercase):
+        enc_word = "".join(numb_list_to_char_list(
+            get_cesar(
+                string_list_to_numb(word), inti)))
+        dict_find_shift[enc_word] = int(inti)
+
+    return dict_find_shift
+
+
+@decorator
+def create_vigenere_cipher_key(word: str, key_len: int) -> list:
+    key_repeated = []
+    while len(key_repeated) < key_len:
+        key_repeated += word
+    logging.info(key_repeated)
+    result = string_list_to_numb(key_repeated)
+    return result
+
+
+@decorator
+def decode_vigenere_cipher(list_of_encrypted_numbs: str, list_of_key_shifts) -> list:
+    result = []
+    logging.info(list_of_key_shifts)
+    for n, k in zip(list_of_encrypted_numbs, list_of_key_shifts):
+        result.append((n-k) % 26)
+    output_result(numb_list_to_char_list(result))
+    return result
+
+
+def task3():
+    butterscotch_dict = create_dict("butterscotch")
+    lower_input = "".join(input().lower().split())
+    print(lower_input)
+    shift = 0
+    for k, v in butterscotch_dict.items():
+        if lower_input.find(k) > -1:
+            shift = butterscotch_dict[k]
+            logging.warning(f"found shift value of: {shift} , {k}")
+    result: list = numb_list_to_char_list(
+        get_cesar(
+            string_list_to_numb(input().lower().split()), -shift))
+    output_result(result)
+
+
 @decorator
 def main():
     # result = output_result(numb_list_to_char_list(input().split()))
-    result = numb_list_to_char_list(get_cesar(string_list_to_numb(input().split()),3))
-    output_result(result)
+    # len_key = input()
+    raw_key = input("key:")
+    numb_encrypted = input("to encrypt:").replace(" ", "x")
+    numb_encrypted = string_list_to_numb(numb_encrypted)
+    numb_vigenere_cipher_key = create_vigenere_cipher_key(raw_key, len(numb_encrypted))
+    decode_vigenere_cipher(numb_encrypted, numb_vigenere_cipher_key)
 
 
 if __name__ == "__main__":
